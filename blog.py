76924d7dbea909b4641a3a64e2333a52c1eeda7f
Mauro Allegrini <mauro@binomia.com>
2014-12-20 22:28:36 +0100

    first beta

--- blog.py
+++ blog.py
@@ -1,5 +1,63 @@
 #
 
+from flask import Flask, render_template, request, session, \
+    flash, redirect, url_for, g
 
+from functools import wraps
+
+import sqlite3
+
+DATABASE = 'blog.db'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+SECRET_KEY = 'hard_to_guess'
+
+app = Flask(__name__)
+
+app.config.from_object(__name__)
+
+def login_required(test):
+    @wraps(test)
+    def wrap(*args,**kwargs):
+        if 'logged_in' in session:
+            return test(*args,**kwargs)
+        else:
+            flash('You need to login first')
+            return redirect(url_for('login'))
+    return wrap
+
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in',None)
+    flash('You were logged out')
+    return redirect(url_for('login'))
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME'] or \
+            request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid Credentials.'
+        else:
+            session['logged_in'] = True
+            return redirect(url_for('main'))
+    return render_template('login.html', error=error)
+
+
+@app.route('/main')
+@login_required
+def main():
+    g.db = connect_db()
+    cur = g.db.execute('select * from posts')
+    posts = [dict(title=row[0],post=row[1]) for row in cur.fetchall()]
+    g.db.close()
+    return render_template('main.html',posts=posts)
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
 
